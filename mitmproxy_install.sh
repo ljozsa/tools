567ca69bc72d1e874919d74a74e54ac51c5cbcd8
Ladislav Jozsa <ljozsa@redhat.com>
2015-10-29 13:45:16 +0100

    mitmproxy_install.sh: installing missing dependency xsel from EPEL

--- mitmproxy_install.sh
+++ mitmproxy_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # install pip
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 curl --fail https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 if [ -e get-pip.py ]; then
 	python get-pip.py
 else
-	rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
     yum -y install python-pip
 	pip install --upgrade pip
 fi
 
 # prepare mitmproxy dependencies
 yum -y remove pyOpenSSL
-yum -y install gcc python-devel libxml2-devel libxslt-devel libffi-devel openssl-devel libjpeg-turbo-devel tmux
+yum -y install gcc python-devel libxml2-devel libxslt-devel libffi-devel openssl-devel libjpeg-turbo-devel tmux xsel
 
 # install mitmproxy
 pip install mitmproxy
